fcb80955c2f726e418d74573c59485e1c63a4a60
Charles-Antoine de Thibault <charles-Antoine.dethibault@edhec.com>
2020-02-13 12:20:58 +0000

    update Coursera final

---
+++ CourseraFinal/CourseraIBM/lib/python3.7/site-packages/catboost/__init__.py
@@ -0,0 +1,12 @@
+from .core import FeaturesData, EFstrType, Pool, CatBoost, CatBoostClassifier, CatBoostRegressor, CatBoostError, cv, train, sum_models, _have_equal_features  # noqa
+from .version import VERSION as __version__  # noqa
+__all__ = ['FeaturesData', 'EFstrType', 'Pool', 'CatBoost', 'CatBoostClassifier', 'CatBoostRegressor', 'CatBoostError', 'CatboostError', 'cv', 'train', 'sum_models', '_have_equal_features']
+
+# API compatibility alias.
+CatboostError = CatBoostError
+
+try:
+    from .widget import MetricVisualizer  # noqa
+    __all__.append('MetricVisualizer')
+except:
+    pass
